7b8d020a9224d56d8a870b8ea50d21b47da636f6
ainhoa-ux <ainhoa.fustel@alumnos.udit.es>
2025-11-20 18:42:49 +0100

    cursor, menu

--- JS/script.js
+++ JS/script.js
@@ -123,3 +123,13 @@ $(function() {
     $(this).siblings(".extra").slideToggle();
   });
 });
+
+
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+document.body.appendChild(cursor);
+
+document.addEventListener("mousemove", e => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+});
